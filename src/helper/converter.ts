ad12973c1c585b38dd96916e1032f28c4d759820
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-05-10 14:39:28 +0200

    Refactor converter.ts

--- src/helper/converter.ts
+++ src/helper/converter.ts
@@ -1,13 +1,13 @@
-const formatting = (timeStamp: number) => {
-	const val = timeStamp.toString() + '000';
-	const ts = parseInt(val, 10);
-	return ts;
-};
-
 export const convertHumanDateToUnixTimestamp = (
-	from: { year: any; month: any; day: any },
-	to: { year: any; month: any; day: any }
-) => {
+	from: { year: number; month: number; day: number },
+	to: { year: number; month: number; day: number }
+): { from: number; to: number } => {
+	const formatting = (timeStamp: number) => {
+		const val = timeStamp.toString() + '000';
+		const ts = parseInt(val, 10);
+		return ts;
+	};
+
 	let toTStamp = new Date(
 		Date.UTC(from.year, from.month - 1, from.day, 6, 0, 0)
 	);
@@ -20,31 +20,28 @@ export const convertHumanDateToUnixTimestamp = (
 	};
 };
 
-
-export const convertUnixDateToHumanDate = (unixTimestamps: any) => {
-    
-    const humanDateFormatting = (unix_timestamp: any) => {
+export const convertUnixDateToHumanDate = (unixTimestamps: number[]): string[] => {
+	const humanDateFormatting = (unix_timestamp: number) => {
 		var date = new Date(unix_timestamp).toString().split(' ');
 		return date[2] + '.' + date[1];
-    };
-    
+	};
+
 	const humanDates = unixTimestamps.map((timestamps: any) => {
 		return humanDateFormatting(timestamps[0]);
 	});
 	return humanDates;
 };
 
-
-export const convertBytesToGbps = (bytes: any, decimals = 3) => {
+export const convertBytesToGbps = (bytes: any, decimals = 3): any[] | any => {
 	const k = 1024;
 	const dm = decimals < 0 ? 0 : decimals;
 	if (bytes.length) {
-		const gbps = bytes.map((el: any) => {
-			if (el[1] === 0) return '0 Bytes';
-			const i = Math.floor(Math.log(el[1]) / Math.log(k));
-			return parseFloat((el[1] / Math.pow(k, i)).toFixed(dm));
+		const gigabytePerSecond = bytes.map((bytes: number[]) => {
+			if (bytes[1] === 0) return '0 Bytes';
+			const i = Math.floor(Math.log(bytes[1]) / Math.log(k));
+			return parseFloat((bytes[1] / Math.pow(k, i)).toFixed(dm));
 		});
-		return gbps;
+		return gigabytePerSecond;
 	}
 
 	// special case for sum, here bytes is not an array
